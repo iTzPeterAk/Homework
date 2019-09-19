35eda2ef05aea67ee78b14d6f01d23579569c2b9
iTzPeterAk <55443376+iTzPeterAk@users.noreply.github.com>
2019-09-19 08:34:36 -0400

    Add files via upload

---
+++ 2019-09-19-Homework/2019-09-19-Homework/main.cpp
@@ -0,0 +1,23 @@
+// Peter Akindayo
+// Intro to Computer Science
+// Period 1
+// Mr. Williams.
+// 2019-09-19-Homework 
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Name: Peter" << std::endl
+	          << "Class Schedule" << std::endl
+	          << "Period 1: Intro to CS" << std::endl
+	          << "Period 2: Health" << std::endl
+			  << "Period 3: French I" << std::endl
+			  << "Period 4: Earth Science" << std::endl
+			  << "Period 5: Geometry" << std::endl
+			  << "Period 6: Lunch" << std::endl
+			  << "Period 7: Gym" << std::endl
+			  << "Period 8: Global History" << std::endl
+			  << "Period 9: English 10 Honors" << std::endl
+			  << "Period 10: House" << std::endl;
+}       
